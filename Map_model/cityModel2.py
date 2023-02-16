bcfdae043432355e2fb55ea4488811e53245e830
MavMaverick <97918848+MavMaverick@users.noreply.github.com>
2023-02-15 18:20:41 -0600

    Cleaned up navigate() func, it can now traverse all connected objects in the road network while keeping track of where its' last stop was, whether it can go on a ramp or not based on its location.
    Removed str def for 'connections' on all classes, it just printed their mem location anyway. Added r_type attribute to ramps, which tells if it's on or off.
    
    Added 'Truck' class, beginning to implement simpy. Imported simpy.

--- Map_model/cityModel2.py
+++ Map_model/cityModel2.py
@@ -1,15 +1,16 @@
 import math  # Needed for Haversine formula for calculating distance between intersection cords
-
+import simpy
 
 class Ramp:  # When a road connects to a highway, you need to know what ramp is there.
     # pos stands for 'position',it consists of the latitude and longitude as ( , )
-    def __init__(self, name, pos, connections):
+    def __init__(self, name, pos, connections, r_type):
         self.name = name
         self.pos = pos
         self.connections = connections
+        self.r_type = r_type
 
     def __str__(self):  # Prints object attributes, still figuring this out
-        return f"{self.name}, {self.pos}, {self.connections}"
+        return f"{self.name}, {self.pos}"
 
 
 class Intersection:  # When a road connects to a road, you need to know what intersection is there.
@@ -20,7 +21,7 @@ class Intersection:  # When a road connects to a road, you need to know what int
         self.connections = connections
 
     def __str__(self):
-        return f"{self.name}, {self.pos}, {self.connections}"
+        return f"{self.name}, {self.pos}"
 
 
 class HwyIntersection:  # When a highway connects to a highway, you need to know what highway_intersection is there.
@@ -30,7 +31,7 @@ class HwyIntersection:  # When a highway connects to a highway, you need to know
         self.connections = connections
 
     def __str__(self):
-        return f"{self.name}, {self.pos}, {self.connections}"
+        return f"{self.name}, {self.pos}"
 
 
 class Highway:  # Creates the highways
@@ -39,7 +40,7 @@ class Highway:  # Creates the highways
         self.connections = connections
 
     def __str__(self):
-        return f"{self.name}, {self.connections}"
+        return f"{self.name}"
 
 
 class Road:  # Creates the major roads
@@ -48,7 +49,20 @@ class Road:  # Creates the major roads
         self.connections = connections
 
     def __str__(self):
-        return f"{self.name}, {self.connections}"
+        return f"{self.name}"
+
+
+class Truck:
+    def __init__(self, env, name):
+        self.env = env
+        self.name = name
+        self.action = env.process(self.run())
+
+    def run(self):
+        while True:
+            print('Truck is trucking, time now is {}'.format(env.now))
+
+            yield self.env.timeout(1)
 
 
 # distance() checks for same road, then reports distance between the two intersections in km based on long and lat
@@ -73,10 +87,11 @@ def distance(intersection1, intersection2):  # You can use Intersection, Ramp, o
     lat2, lon2 = intersection2.pos
     radius = 6371  # Earth's radius in km
 
-    dlat = math.radians(lat2-lat1)
-    dlon = math.radians(lon2-lon1)
-    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    dlat = math.radians(lat2 - lat1)
+    dlon = math.radians(lon2 - lon1)
+    a = math.sin(dlat / 2) * math.sin(dlat / 2) + math.cos(math.radians(lat1)) * math.cos(
+        math.radians(lat2)) * math.sin(dlon / 2) * math.sin(dlon / 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     d = radius * c
     #  This formula for Haversine was taken from We3schools
 
@@ -103,30 +118,32 @@ def see_connections(obj):  # Loops through an object's connections and prints th
     print("\n")
 
 
-def navigate(road):  # work in progress method for traveling objects
-    # Take current road, find connections, let user choose location, record choice, update current, loop at new road
-    while True:
-        choices = []
-        print("You are currently at {}".format(road.name))
-        print("You can travel to any of these intersections on this road:  ")
-        for i in road.connections:
-            choices.append(i)
-            print(i.name, end=", ")
-        print("\n")
-        usr_choice = input("    Enter index 0 ... n to select destination\n")
-        usr_choice = int(usr_choice)
-        print("You have chosen ", choices[usr_choice].name)
-        road = choices[usr_choice].name
-        print("You can travel to any of these roads from this intersection:  ")
-        choices = []
 
-        for x in road.connections:
-            choices.append(x)
-            print(x.name, end=", ")
-        print("\n")
-        usr_choice = input("    Enter index 0 ... n to select destination\n")
-        usr_choice = int(usr_choice)
+
+def navigate(road):  # Lets you traverse connected road network objects
+    # Take current road, find connections, let user choose location, record choice, update current, loop at new road
+    curr = road  # stores object passed by parameter
+    prev_rd = curr  # stores last traveled road
+    while True:
+        choices = []  # stores list
+        print("You are currently at {}".format(curr.name), "You can travel to any of these connections:  ")
+        for i in curr.connections:  # creates a list of user options based on connected objects
+            if isinstance(i, Ramp) and i.r_type == True and isinstance(prev_rd, HwyIntersection):
+                # if i is a ramp AND an ONRamp AND previous road is a highway intersect; then PASS
+                pass
+            elif isinstance(i, Ramp) and i.r_type == False and isinstance(prev_rd, Highway):
+                # if i is a ramp AND an offRamp AND previous road is a highway; then append
+                choices.append(i)  # add object to choice list
+                print(i.name, end=", ")  # print same line
+            elif i == prev_rd:  # check if i is the connection you were last on
+                pass  # skip appending to the choice list
+            else:
+                choices.append(i)  # add object to choice list
+                print(i.name, end=", ")  # print same line
+        usr_choice = int(input("    `Enter index 0 ... n to select destination\n"))
         print("You have chosen ", choices[usr_choice].name)
+        prev_rd = curr  # current becomes previous
+        curr = choices[usr_choice]  # current becomes user choice
 
 
 # Highway instantiation (2 highways)
@@ -140,8 +157,8 @@ road4 = Road('road4', [])
 road5 = Road('road5', [])
 road6 = Road('road6', [])
 # Intersection instantiation (8 intersections)
-inter1 = Intersection('inter1', (51.5074, 0.1278), [road1, road2])
-inter2 = Intersection('inter2', (40.7128, 74.0060), [road1, road3])
+inter1 = Intersection('inter1', (44.154667, -93.973167), [road1, road2])
+inter2 = Intersection('inter2', (44.142, -93.992), [road1, road3])
 inter3 = Intersection('inter3', (1, 1), [road3, road4])
 inter4 = Intersection('inter4', (1, 1), [road4, road5])
 inter5 = Intersection('inter5', (1, 1), [road2, road5])
@@ -149,11 +166,11 @@ inter6 = Intersection('inter6', (1, 1), [road4, road5])
 inter7 = Intersection('inter7', (1, 1), [road4, road6])
 inter8 = Intersection('inter8', (1, 1), [road5, road6])
 # Ramp instantiation (5 ramps)
-onramp1 = Ramp('onramp1', (1, 1), [road1, hwy1])
-onramp2 = Ramp('onramp2', (1, 1), [road5, hwy2])
-offramp1 = Ramp('offramp1', (1, 1), [hwy1, road6])
-offramp2 = Ramp('offramp2', (1, 1), [hwy1, road3])
-offramp3 = Ramp('offramp3', (1, 1), [hwy2, road5])
+onramp1 = Ramp('onramp1', (1, 1), [road1, hwy1], True)
+onramp2 = Ramp('onramp2', (1, 1), [road5, hwy2], True)
+offramp1 = Ramp('offramp1', (1, 1), [hwy1, road6], False)
+offramp2 = Ramp('offramp2', (1, 1), [hwy1, road3], False)
+offramp3 = Ramp('offramp3', (1, 1), [hwy2, road5], False)
 # Highway Intersection instantiation (1 hwy intersect)
 hwyinter1 = HwyIntersection("Hw1_Hw2", (1, 1), [hwy1, hwy2])
 
@@ -167,14 +184,13 @@ road6.connections += [inter7, inter8, offramp1]
 hwy1.connections += [onramp1, offramp1, offramp2, hwyinter1]
 hwy2.connections += [onramp2, offramp3, hwyinter1]
 
+#  Create the simpy environment
+env = simpy.Environment()
 
-see_connections(hwy1)
-see_connections(inter1)
-see_connections(inter3)
-see_connections(road5)
-see_connections(offramp3)
+#  Instantiate truck object
+truck1 = Truck(env, 'truck1')
 
-distance_func_test()
+env.run(until=5)
 
+print(distance(inter1, inter2))
 navigate(road1)
-
